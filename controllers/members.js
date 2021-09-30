3a82bb85c2241b62ef4a353c15c85886afb6bb15
Martin de Amorin <martindeamorin@gmail.com>
2021-09-30 21:38:45 +0000

    Merged in OT75-92/delete-member (pull request #30)
    OT75-92/delete member
    
    * adding route and controller for delete member
    
    * delete controller finished
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-server into OT75-92/delete-member
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-server into OT75-92/delete-member
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-server into OT75-92/delete-member
    
    
    Approved-by: Dario Velez
    Approved-by: Fabian Liuni

--- controllers/members.js
+++ controllers/members.js
@@ -8,7 +8,23 @@ const membersController = {
         }catch(err){
             return res.status(500).json({ok: false, msg: err.message});
         }
+    },
+    deleteMember: async (req, res) => {
+        const {id} = req.params;
+        let memberExists = await Member.findByPk(id);
+        if(memberExists){
+            try{
+                let deletedMember = await Member.destroy({where: {id}});
+                return res.status(200).json({ok: true, data: deletedMember[0]});
+            }catch(err){
+                return res.status(500).json({ok: false, msg: err.message});
+            }
+        }
+
+        return res.status(404).json({ok: false, msg: `Cannot find member with id ${id}`});
+
     }
+
 }
 
 module.exports = membersController;
--- routes/members.js
+++ routes/members.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getMembers} = require("../controllers/members");
+const {getMembers, deleteMember} = require("../controllers/members");
 
 router.get("/", getMembers);
+router.delete("/:id", deleteMember);
 
 module.exports = router;
