f8ad321c2b5c8fd3c1fc7484d2890988bcf78e31
Martin de Amorin <martindeamorin@gmail.com>
2021-10-01 00:29:12 +0000

    Merged in OT75-91/update-member (pull request #34)
    OT75-91/update member
    
    * creation of endpoint for update user
    
    * fixed response
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-server into OT75-91/update-member
    
    * merging with develop
    
    
    Approved-by: Dario Velez
    Approved-by: Facundo Gaston Lazcano Colodrero

--- controllers/members.js
+++ controllers/members.js
@@ -30,7 +30,20 @@ const membersController = {
         }
 
         return res.status(404).json({ok: false, msg: `Cannot find member with id ${id}`});
+    },
+    updateMember: async (req, res) => {
+        const {id} = req.params;
+        let memberExists = await Member.findByPk(id);
+        if(memberExists){
+            try{
+                let updatedMember = await Member.update(req.body, {where: {id}});
+                return res.status(200).json({ok: true, data: updatedMember[0]});
+            }catch(err){
+                return res.status(500).json({ok: false, msg: err.message});
+            }
+        }
 
+        return res.status(404).json({ok: false, msg: `Cannot find member with id ${id}`});
     }
 
 }
--- routes/members.js
+++ routes/members.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {getMembers, deleteMember, createMember} = require("../controllers/members");
+const {getMembers, deleteMember, createMember, updateMember} = require("../controllers/members");
 const { validate } = require('../middlewares/validate');
 const memberSchema = require('../validation-schemas/member');
 
 
 router.get("/", getMembers);
-router.delete("/:id", deleteMember);
 router.post("/", validate(memberSchema), createMember);
+router.put("/:id",validate(memberSchema), updateMember);
+router.delete("/:id", deleteMember);
 
 module.exports = router;
