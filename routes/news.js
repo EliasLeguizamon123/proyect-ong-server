61deb3ead0f216574de90367388cf8d684268b11
Ruben Gianelli <rubengianelli8@gmail.com>
2021-10-01 00:26:39 +0000

    Merged in OT75-45 (pull request #31)
    OT75-45
    
    * controller to obtain list of entries type news added
    
    * 'news' route created
    
    
    Approved-by: Dario Velez
    Approved-by: Martin de Amorin

--- routes/news.js
+++ routes/news.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 
 const router = Router();
 
-const { deleteEntries, postEntries, getNewById } = require('../controllers/news.js');
+const { deleteEntries, postEntries, getNewById, getEntries } = require('../controllers/news.js');
 
+router.get('/', getEntries);
 router.get('/:id', getNewById);
 router.delete('/', deleteEntries);
 router.post('/', postEntries);
