e93b773bdc55073aa5805cf52281bbf8a8b59efc
Milagros Lechares <80794759+mflec@users.noreply.github.com>
2021-10-07 13:52:25 -0300

    adding jwt logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,10 @@
 Imports
 */
 const bcrypt = require('bcrypt')
-const { User } = require('../models/index')
+const jwt = require('jsonwebtoken')
+const { User, Role } = require('../models/index')
+
+const { JWT_SECRET_KEY } = process.env
 
 /*
 Controllers
@@ -13,7 +16,10 @@ const authLogin = async (req, res) => {
     const user = await User.finOne({
       where: {
         email
-      }
+      },
+      include: [{
+        model: Role
+      }]
     })
 
     if (!user) throw new Error('The email is not registered.')
@@ -23,9 +29,22 @@ const authLogin = async (req, res) => {
 
     if (!passwordMatch) throw new Error("Passwords don't match")
 
+    const userData = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.lastName,
+      image: user.image ? user.image : null,
+      roles: user.roles ? user.roles : null
+    }
+
+    const token = jwt.sign(userData, JWT_SECRET_KEY, {
+      expiresIn: 60 * 60 * 24 // 60*60*24s = 1day
+    })
+
     return res.status(200).json({
       ok: true,
-      data: user
+      data: userData,
+      token
     })
   } catch (error) {
     return res.status(500).json({
---
+++ middlewares/verifyAuth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+const { JWT_SECRET_KEY } = process.env
+
+const verifyToken = async (req, res, next) => {
+  try {
+    // get the token from the headers:
+    const token = req.headers['x-access-token']
+    if (!token) throw new Error('no Token aws Provided')
+
+    // decode the token
+    const decoded = jwt.verify(token, JWT_SECRET_KEY)
+
+    // save the user id
+    req.user = decoded
+
+    return next()
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: error.message
+    })
+  }
+}
+
+module.exports = {
+  verifyToken
+}
