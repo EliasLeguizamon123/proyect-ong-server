75d2ff5ca4262bf2683a3a45a0c0850ae181c3df
Ruben Gianelli <rubengianelli8@gmail.com>
2021-10-07 23:38:25 +0000

    Merged in OT75-79 (pull request #51)
    OT75-79
    
    * model and migration created for organization links
    
    * seed organizationLink added
    
    * modified organization controller
    
    
    Approved-by: Dario Velez
    Approved-by: Fabian Liuni

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,8 +1,14 @@
-const { Organization } = require('../models/index')
+const { Organization, OrganizationLink } = require('../models/index')
 
 const getPublicData = async (req, res) => {
   try {
-    const PublicData = await Organization.findAll({})
+    const PublicData = await Organization.findAll({
+      include: [
+        {
+          model: OrganizationLink
+        }
+      ]
+    })
     return res.status(200).json({
       ok: true,
       data: PublicData
