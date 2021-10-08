c03250d7eff560a6a7f2d41dac82ead11a16775b
Dario Velez Roman <dvelezroman@gmail.com>
2021-10-07 22:49:44 -0300

    fix bcrypt functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ Imports
 */
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { User, Role } = require('../models/index')
+const { User } = require('../models/index')
 
 const { JWT_SECRET_KEY } = process.env
 
@@ -13,19 +13,16 @@ Controllers
 const authLogin = async (req, res) => {
   try {
     const { email, password } = req.body
-    const user = await User.finOne({
+    const user = await User.findOne({
       where: {
         email
-      },
-      include: [{
-        model: Role
-      }]
+      }
     })
 
     if (!user) throw new Error('The email is not registered.')
 
     /*   method provided by bcrypt to compare passwords:   */
-    const passwordMatch = bcrypt.compareSync(password, user.password)
+    const passwordMatch = bcrypt.compare(password, user.password)
 
     if (!passwordMatch) throw new Error("Passwords don't match")
 
