31d194560d04bb8a56db95823508c3ac35226306
Milagros Lechares <mifilechares@gmail.com>
2021-10-12 18:40:36 +0000

    Fusionado en OT75-33 (solicitud de integración #50)
    OT75-33 Add JWT token to Login
    
    
    * way to get the token modified
    
    * fix bcrypt functions
    
    * fix the error when no token in header
    
    * Merge branch 'OT75-33' of https://bitbucket.org/alkemy-dev/t75-server into OT75-33

---
+++ middlewares/verifyAuth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+
+const { JWT_SECRET_KEY } = process.env
+
+const verifyToken = async (req, res, next) => {
+  try {
+    // get the token from the headers:
+    const token = req.headers.authorization.split(' ')[1]
+
+    if (!token) {
+      res.status(401).json({
+        ok: false,
+        msg: 'No Auth Provided'
+      })
+    }
+
+    // decode the token
+    const decoded = jwt.verify(token, JWT_SECRET_KEY)
+
+    // save the user info
+    req.user = decoded
+
+    next()
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: error.message
+    })
+  }
+}
+
+module.exports = {
+  verifyToken
+}
