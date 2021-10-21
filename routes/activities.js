cd7fb7f984d800fa44473380bab6f091eadf4259
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2021-10-21 23:53:27 +0000

    Merged in OT75-117 (pull request #76)
    OT75-117
    
    * testing in activities routes
    
    * fix put activity who non exist
    
    
    Approved-by: Dario Velez
    Approved-by: Facundo Gaston Lazcano Colodrero

--- routes/activities.js
+++ routes/activities.js
@@ -6,6 +6,7 @@ const { Router } = require('express')
 const { validate } = require('../middlewares/validate')
 const schemas = require('../validation-schemas/activities')
 const activityController = require('../controllers/activities')
+const { verifyToken } = require('../middlewares/verifyAuth')
 
 /*
 Routes to handle activities
@@ -14,18 +15,18 @@ Routes to handle activities
 const router = Router()
 
 // PUT activities
-router.put('/:id', activityController.update)
+router.put('/:id', verifyToken, activityController.update)
 
 // POST /activities
-router.post('/', validate(schemas.add), activityController.add)
+router.post('/', verifyToken, validate(schemas.add), activityController.add)
 
 // GET /activities
 router.get('/', activityController.getAll)
 
 // GET /activities/:id
-router.get('/:id', validate(schemas.onlyParam), activityController.getOne)
+router.get('/:id', activityController.getOne)
 
 // DELETE /activities/:id
-router.delete('/:id', validate(schemas.onlyParam), activityController.delete)
+router.delete('/:id', verifyToken, activityController.delete)
 
 module.exports = router
