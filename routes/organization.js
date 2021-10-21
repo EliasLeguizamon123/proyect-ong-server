c655da61c859d2c7d960b4c615e8e97cb5fe3953
Facundo Gaston Lazcano Colodrero <faculazcano14@gmail.com>
2021-10-21 22:36:23 +0000

    Merged in OT75-50 (pull request #71)
    OT75-50
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-server/src/master into fix-test-testimonials
    
    * create patch controller for edit organization.
    
    
    * add routes to handle organizations links
    
    
    Approved-by: Dario Velez
    Approved-by: Elias Leguizamon

--- routes/organization.js
+++ routes/organization.js
@@ -2,9 +2,21 @@ const { Router } = require('express')
 
 const router = Router()
 
-const { getPublicData } = require('../controllers/organization')
+const {
+  getPublicData,
+  patchOrganization,
+  postNewLink,
+  deleteLink
+} = require('../controllers/organization')
+const { verifyToken } = require('../middlewares/verifyAuth')
 
 // Route GET / contacts
 router.get('/:id', getPublicData)
+
+router.patch('/:id', verifyToken, patchOrganization)
+
+router.post('/:id/links', verifyToken, postNewLink)
+
+router.delete('/:id/links/:linkid', verifyToken, deleteLink)
 
 module.exports = router
